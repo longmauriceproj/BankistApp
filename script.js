32053df5887744cd4e8f6e5e84829efc6c4cf8e8
Maurice Long <longmaurice255@gmail.com>
2021-12-10 12:01:51 -0800

    Created function to display transactions

--- script.js
+++ script.js
@@ -32,3 +32,50 @@ const account4 = {
 };
 
 const accounts = [account1, account2, account3, account4];
+
+//Elements - select them all here
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerTransactions = document.querySelector('.transactions');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+//NOTE: better to pass data into function instead of writing in global context
+const displayTransactions = function (transactions) {
+  //Empty container - innerHTML is similar to .textContent but returns all HTML code
+  containerTransactions.innerHTML = '';
+  //loop through account transactions to create transaction row elements
+  transactions.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="transactions__row">
+        <div class="transactions__type transactions__type--${type}">
+      ${i + 1} ${type}
+        </div>
+        <div class="transactions__value">${mov}</div>
+    </div>
+  `;
+    //insert transaction row elements into container
+    containerTransactions.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayTransactions(account1.transactions);
